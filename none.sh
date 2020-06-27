03af0e36f7e1b28d35a16b5d84699958554afa9d
AnggaJaunk98 <65926127+Jaunk98@users.noreply.github.com>
2020-06-28 04:32:16 +0700

    Add files via upload

---
+++ none.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+clear
+toilet -F slant -f gay Tools.v6
+echo "=============================="
+echo " Author               : AnggaJaunk"
+echo " Youtube              : Termux Pemula"
+echo " Facebook           : Angga Jaunk98"
+echo "=============================="
+echo ""
+echo "selamat datang di tools"
+echo "[1] Spam Sms"
+echo "[2] Bot Facebook"
+echo "[3] Exit"
+read -p "Masukan Pilihan :" sip
+case $sip in
+1)
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+;;
+2)
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+echo "succes"
+;;
+3)
+exit
+;;
+esac
